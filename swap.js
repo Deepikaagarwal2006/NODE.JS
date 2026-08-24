8b4de375441123d79830edfa04b2047a2171e87b
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-08-24 09:27:57 +0530

    swap.js

---
+++ swap.js
@@ -0,0 +1,27 @@
+// Program to swap two variables using a third (temporary) variable
+
+// let a = 5;
+// let b = 10;
+
+// console.log("Before swap: a =", a, ", b =", b);
+
+// // Using a third variable to swap
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log("After swap: a =", a, ", b =", b);
+
+
+console.log("start");
+setTimeout(() => {
+    console.log("Timeout function executed after 2 seconds");
+}
+, 10);
+
+Promise.resolve().then(() => {
+    console.log("Promise resolved");
+});
+console.log("end");
+
+
