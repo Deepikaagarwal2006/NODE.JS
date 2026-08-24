080a11a6382f122b855689147ce4f0a4fddc5085
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-08-24 10:16:29 +0530

    commonjs.js

---
+++ commonjs.js
@@ -0,0 +1,11 @@
+// const calculate_area = require('./main')
+// const calculate_perimeter= require('./main')
+
+
+
+// console.log(calculate_area(1));
+// console.log(calculate_perimeter(1));
+
+
+import isVote from './esm'
+console.log(isVote(24));
