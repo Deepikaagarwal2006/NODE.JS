bb264e9de27217848ac5dba648460f932f8904cb
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-08-31 20:28:49 +0530

    event.js

---
+++ event.js
@@ -0,0 +1,28 @@
+// import fs from "fs";
+// setTimeout(()=> {
+//     console.log("setTimeout");
+// }, 1000);
+
+// fs.readFile("intro.txt", "utf8" , (err, data) => {
+//     console.log("File read completed");   
+// });
+// setInterval(()=>{
+//     console.log("set
+// Interval after 5ms");
+// }, 500);
+
+
+// setImmediate(()=>{
+//     console.log("Set Immediate");
+// });
+
+
+fs.readFile("intro.txt", "utf8" , (err, data) => {
+    console.log("File read completed"); 
+    setTimeout(()=>{
+        console.log("setTimeout");
+    }, 0);
+    setImmediate(()=>{
+        console.log("Set Immediate");
+    });
+});
