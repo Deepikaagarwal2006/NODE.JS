022d2d14ea1a5a31add0255712e7c19df27c5823
Deepikaagarwal2006 <agarwaldeepika2006@gmail.com>
2026-09-13 16:59:23 +0530

    watch.js

---
+++ node_watch/watch.js
@@ -0,0 +1,13 @@
+const fs =  require('fs');
+fs.watch("intro.txt" ,(eventType,filename)=>{
+    console.log("Event: " , eventType);
+    console.log("Filename: " , filename);
+})
+
+
+setTimeout(() => {
+     watcher.close()
+    console.log("Watched Closed")
+} , 5000)
+    
+    
